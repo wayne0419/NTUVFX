6dfada838787d96b60511c8690435ab86141aac4
CheWei Wang <ddwayne@umich.edu>
2022-04-27 20:40:27 +0800

    update readme

--- proj2/main.py
+++ proj2/main.py
@@ -43,45 +43,45 @@ proj_img_pyramids, description_pyramids = msop.msop(images, focal_lengths, pyram
 # Do Feature -Matching to get paired_points_pyramids
 paired_points_pyramids = stitching.build_paired_points_pyramids(description_pyramids, ratio_threshold=0.5) #TODO may use ratio_threshold = 0.8
 
-# # Plot Result #######################################################################
-# columns = PYRAMID_DEPTH
-# rows = N_IMG
-# fig=plt.figure(figsize=(columns*5, rows*2))
-# for i in range(0, rows-1):
+# Plot Result #######################################################################
+columns = PYRAMID_DEPTH
+rows = N_IMG
+fig=plt.figure(figsize=(columns*5, (rows-1)*2))
+for i in range(0, rows-1):
 	
-# 	for p in range(columns):
-# 		proj_img = proj_img_pyramids[i][p]
-# 		proj_img_next = proj_img_pyramids[i+1][p]
-# 		paired_points = paired_points_pyramids[i][p]
+	for p in range(columns):
+		proj_img = proj_img_pyramids[i][p]
+		proj_img_next = proj_img_pyramids[i+1][p]
+		paired_points = paired_points_pyramids[i][p]
 
-# 		fig.subplots_adjust(wspace=0.2)
-# 		fig.add_subplot(rows, columns, i*PYRAMID_DEPTH+1+p)
+		fig.subplots_adjust(wspace=0.2)
+		fig.add_subplot(rows, columns, i*PYRAMID_DEPTH+1+p)
 		
-# 		(hA, wA) = proj_img.shape[:2]
-# 		(hB, wB) = proj_img_next.shape[:2]
+		(hA, wA) = proj_img.shape[:2]
+		(hB, wB) = proj_img_next.shape[:2]
 
-# 		# Plot image
-# 		vis = np.zeros((max(hA, hB), wA + wB, 3), dtype="uint8")
-# 		vis[0:hA, 0:wA] = proj_img
-# 		vis[0:hB, wA:] = proj_img_next
-# 		plt.imshow(cv2.cvtColor(vis, cv2.COLOR_BGR2RGB))
+		# Plot image
+		vis = np.zeros((max(hA, hB), wA + wB, 3), dtype="uint8")
+		vis[0:hA, 0:wA] = proj_img
+		vis[0:hB, wA:] = proj_img_next
+		plt.imshow(cv2.cvtColor(vis, cv2.COLOR_BGR2RGB))
 
-#         # Plot points, lines
-# 		for (apoint, bpoint) in paired_points:
+        # Plot points, lines
+		for (apoint, bpoint) in paired_points:
 
-# 			color = np.random.randint(0, high=255, size=(3,))
-# 			# Plot point in image A, left, i
-# 			plt.plot(apoint.x, apoint.y, 'ro')
-# 			ptA = (int(apoint.y), int(apoint.x))
-# 			# Plot point in image B, right, i+1
-# 			plt.plot(bpoint.x + wA, bpoint.y, 'ro')
-# 			ptB = (int(bpoint.y), int(bpoint.x + wA))
+			color = np.random.randint(0, high=255, size=(3,))
+			# Plot point in image A, left, i
+			plt.plot(apoint.x, apoint.y, 'ro')
+			ptA = (int(apoint.y), int(apoint.x))
+			# Plot point in image B, right, i+1
+			plt.plot(bpoint.x + wA, bpoint.y, 'ro')
+			ptB = (int(bpoint.y), int(bpoint.x + wA))
 
-# 			# Plot line between points
-# 			plt.plot([ptA[1], ptB[1]], [ptA[0], ptB[0]])
+			# Plot line between points
+			plt.plot([ptA[1], ptB[1]], [ptA[0], ptB[0]])
 
-# plt.show()
-# ####################################################################################
+plt.show()
+####################################################################################
 
 
 
@@ -133,7 +133,7 @@ combined_image = stitching.combine_images(proj_images, B2A_translations)
 # Plot Result ###########################################################
 plt.imshow(cv2.cvtColor(combined_image, cv2.COLOR_BGR2RGB))
 plt.show()
-cv2.imwrite("report_material/10_result_mountain.png",combined_image)
+# cv2.imwrite("report_material/10_result_mountain.png",combined_image)
 #########################################################################
 		
 
